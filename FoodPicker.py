9486accd6a57e817dea81c5f2de58dd99cabee83
efahy2313 <56339033+efahy2313@users.noreply.github.com>
2020-12-08 18:36:51 -0700

    Delete food from list when not wanted and checks for an empty list.

--- FoodPicker.py
+++ FoodPicker.py
@@ -1,5 +1,5 @@
 # Author: Eric Fahy
-# Created: 11/30/2020
+# Created: 12/8/2020
 # Last Modified 11/30/2020
 # Description: This driver program is designed to help me and my girlfriend where to eat.
 # A list of restaurants will be held in an array. The program will randomly pick one restaurant out of the list.
@@ -20,22 +20,33 @@ def random_restaurant(res_list):
 
 
 def main():
-    restaurants = ['McDonalds', 'In N Out', 'Canes', 'Peter Piper', 'Barros', "Panda", "Shake Shack", "Jersey Mikes",
+    restaurants = ["McDonalds", "In N Out", "Canes", "Peter Piper", "Barros", "Panda", "Shake Shack", "Jersey Mikes",
                    "Texas Roadhouse", "PF Changs"]
 
     choice = " "
 
-    while choice.lower() != "y":  # Keep choosing restaurant until one is picked
-        print("Does " + str(random_restaurant(restaurants)) + " sound good?")
+    while choice.lower() != "y" and len(restaurants) != 0:  # Keep choosing restaurant until one is picked
+
+        restaurant = random_restaurant(restaurants)
+
+        print("Does " + str(restaurant) + " sound good?")
 
         choice = input("Y or N? \n")
 
-        print(choice.lower())
         if choice.lower() != "y" and choice.lower() != "n":  # Sanitize input
+
             print("Please enter Y (Yes) or N (No). \n")
 
         if choice.lower() == "y":
+
             print("Enjoy!\n")
+
+        elif choice.lower() == "n":
+
+            restaurants.pop(restaurants.index(str(restaurant)))
+
+            if len(restaurants) == 0:
+                print("There are no more restaurants to pick from :( \n")
 
 
 if __name__ == "__main__":
